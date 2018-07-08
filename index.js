e730a7103353b201b8c7e2366383200f5417a20f
qaisaru@icloud.com <qaisaru@icloud.com>
2018-07-08 18:10:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ function destructivelyPrependKitten(name){
 function destructivelyRemoveLastKitten(name){
   kittens.pop(name);
 }
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift();
+}
