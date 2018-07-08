c200cc4e4439b2afb8e70375e29e3e00b318d647
qaisaru@icloud.com <qaisaru@icloud.com>
2018-07-08 17:58:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Add your functions and code here
 
-var kittens = ['Milo', 'Otis', 'Garfield'];
+/*var kittens = ['Milo', 'Otis', 'Garfield'];
 function destructivelyAppendKitten(name) {
   kittens.push(name);
 }
