221c345e39cefdfbfe330fa9524de6d1e2245ff7
qaisaru@icloud.com <qaisaru@icloud.com>
2018-07-08 18:07:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function destructivelyAppendKitten(name) {
   kittens.push(name);
 }
 
-function destructivelyAppendKitten(name){
-  kittens.push(name);
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
 }
